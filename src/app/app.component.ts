4874585ea49b278434749bc0b9ce558cb0680c1b
zip-fa <ikpocc@gmail.com>
2024-07-15 18:14:37 +0300

    bug repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, PLATFORM_ID, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TestCmp} from "./test-cmp";
+import {LoggedCmp} from "./logged-cmp";
+import {isPlatformServer} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestCmp, LoggedCmp],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'defer-loading-bug';
+export class AppComponent implements OnInit {
+  platformId = inject(PLATFORM_ID);
+  isAuthenticated = signal<boolean>(false);
+
+  ngOnInit() {
+    if (isPlatformServer(this.platformId)) {
+      return;
+    }
+
+    setTimeout(() => this.isAuthenticated.set(true), 300);
+  }
 }
---
+++ src/app/logged-cmp.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logged-cmp',
+  standalone: true,
+  imports: [],
+  template: `Logged user profile`
+})
+export class LoggedCmp {
+}
